b393da59f0c821964b5f52c7faf4102f1b41ce2b
Kevin Coffey <kevdougful@gmail.com>
2023-03-16 00:04:08 -0500

    Add Example Components
    I'll probably clean these up later to be more generic but for now they do a decent job of demonstrating how to implement them.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
 
+import { SaasProvider } from '@saas-ui/react';
+
+import {
+    Sidebar,
+    SidebarSection,
+    NavItem,
+} from "@saas-ui/sidebar"
+
+
+import { AppShell } from "@saas-ui/app-shell";
+import { Box, Text } from "@chakra-ui/react";
+
+import ProgramCard from "./components/ProgramCard"
+
 export interface AppProps {
     name: string
-}
+};
 
 export const App = (props: AppProps) => {
-    return <div>
-        <h1>Hello There {props.name}</h1>
-    </div>
-}
+    return (
+        <SaasProvider>
+            <AppShell
+                navbar={
+                    <Box as="header" borderBottomWidth="1px" py="2" px="4">
+                        <Text>Hi</Text>
+                    </Box>
+                }
+                sidebar={
+                    <Sidebar>
+                        <SidebarSection>
+                            <NavItem label="Home" />
+                            <NavItem label="Settings" />
+                        </SidebarSection>
+                    </Sidebar>
+                }
+            >
+                <Box as="main" flex="1" py="2" px="4" overflowY="auto">
+                    Your application content
+                </Box>
+            </AppShell>
+        </SaasProvider>
+    );
+};
